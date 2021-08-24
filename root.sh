befd529f59e9df388e7a7fd98f3ab9973c1ec010
lookoupai <42715712+lookoupai@users.noreply.github.com>
2021-08-24 14:54:11 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -5,7 +5,7 @@ sudo chattr -i /etc/passwd /etc/shadow
 sudo chattr -a /etc/passwd /etc/shadow
 sudo lsattr /etc/passwd /etc/shadow
 
-read -p "×Ô¶¨ÒåROOTÃÜÂë:" mima
+read -p "è‡ªå®šä¹‰ROOTå¯†ç :" mima
 echo root:$mima | sudo chpasswd root
 sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
